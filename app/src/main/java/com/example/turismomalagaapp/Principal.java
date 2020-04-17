59d48e50cb58b068028fd8aa971dd34f77912380
RamonVLL <51043331+RamonVLL@users.noreply.github.com>
2020-04-17 12:02:38 +0200

    //turismoMalaga

--- app/src/main/java/com/example/turismomalagaapp/Principal.java
+++ app/src/main/java/com/example/turismomalagaapp/Principal.java
@@ -11,6 +11,6 @@ public class Principal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    // Ramon Vidal LLamas
+    // Turismo malaga
     //prueba 3.0
 }
