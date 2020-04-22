2cec6ad3e0634e6ca4a7e479334a56866b0931dc
David <davidcurbelomartin@gmail.com>
2020-04-22 19:29:23 +0100

    TextView como botón, con OnClickListener que muestra un mensaje TOAST.

--- app/src/main/java/com/example/turismomalagaapp/Principal.java
+++ app/src/main/java/com/example/turismomalagaapp/Principal.java
@@ -3,13 +3,26 @@ package com.example.turismomalagaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Principal extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = this.findViewById(R.id.textview);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "SOY UN TEXTVIEW", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
