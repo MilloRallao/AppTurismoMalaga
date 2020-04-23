3970d18181c1a5642ca15515a46c4515335c83ec
RamonVLL <51043331+RamonVLL@users.noreply.github.com>
2020-04-23 12:41:52 +0200

    //turismoMalaga con menu lateral funcionando con puntos de informacion

---
+++ app/src/main/java/com/example/turismomalagaapp/ui/puntosInformacion/PuntosInfoFragment.java
@@ -0,0 +1,21 @@
+package com.example.turismomalagaapp.ui.puntosInformacion;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.turismomalagaapp.R;
+
+public class PuntosInfoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_puntos_info, container, false);
+    }
+}
