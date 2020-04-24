8b799a9ed93367b4509fc6526ec9783b5653887b
RamonVLL <51043331+RamonVLL@users.noreply.github.com>
2020-04-24 13:18:00 +0200

    //turismoMalaga Submenu funcionando con problemas en el titulo del toolbar al cambiar del submenu a menu lateral

--- app/src/main/java/com/example/turismomalagaapp/MenuLateralActivity.java
+++ app/src/main/java/com/example/turismomalagaapp/MenuLateralActivity.java
@@ -1,20 +1,30 @@
 package com.example.turismomalagaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import com.example.turismomalagaapp.ui.Submenu.AjustesFragment;
+import com.example.turismomalagaapp.ui.Submenu.ContactoFragment;
+import com.example.turismomalagaapp.ui.Submenu.ValorarFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MenuLateralActivity extends AppCompatActivity {
 
@@ -40,7 +50,7 @@ public class MenuLateralActivity extends AppCompatActivity {
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_event, R.id.nav_ocio, R.id.nav_gastronomia,
-                R.id.nav_cultura,R.id.nav_pInfo, R.id.nav_compras,R.id.nav_cSol)
+                R.id.nav_cultura,R.id.nav_pInfo, R.id.nav_compras,R.id.nav_cSol,R.id.action_ajustes)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
@@ -49,16 +59,51 @@ public class MenuLateralActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_lateral, menu);
-        return true;
-    }
-
-    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    // para el funcionaminto del submenu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_lateral, menu);
+        return true;
+    }
+    // opciones dependiendo del item selecionado del submenu
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+
+        switch (item.getItemId()) {
+            case R.id.action_ajustes:
+                AjustesFragment fragment = new AjustesFragment();
+                FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.nav_host_fragment, fragment);
+                toolbar.setTitle("Ajustes");
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                return true;
+            case R.id.action_contacto:
+                ContactoFragment fragment2 = new ContactoFragment();
+                FragmentTransaction fragmentTransaction2 = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction2.replace(R.id.nav_host_fragment, fragment2);
+                toolbar.setTitle("Contacto");
+                fragmentTransaction2.addToBackStack(null);
+                fragmentTransaction2.commit();
+                return true;
+            case R.id.action_valorar:
+                ValorarFragment fragment3 = new ValorarFragment();
+                FragmentTransaction fragmentTransaction3 = getSupportFragmentManager().beginTransaction();
+                fragmentTransaction3.replace(R.id.nav_host_fragment, fragment3);
+                toolbar.setTitle("Valorar");
+                fragmentTransaction3.addToBackStack(null);
+                fragmentTransaction3.commit();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
---
+++ app/src/main/java/com/example/turismomalagaapp/ui/Submenu/AjustesFragment.java
@@ -0,0 +1,26 @@
+package com.example.turismomalagaapp.ui.Submenu;
+
+import android.os.Bundle;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.example.turismomalagaapp.R;
+
+
+public class AjustesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ajustes, container, false);
+
+    }
+
+
+
+}
