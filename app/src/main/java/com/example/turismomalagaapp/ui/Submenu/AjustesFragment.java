c7e156dd16c82c8666400bad4fd7708daabe2b72
Usuario <davidfebrerpons7@gmail.com>
2020-05-06 12:24:21 +0200

    idioma

--- app/src/main/java/com/example/turismomalagaapp/ui/Submenu/AjustesFragment.java
+++ app/src/main/java/com/example/turismomalagaapp/ui/Submenu/AjustesFragment.java
@@ -1,28 +1,62 @@
 package com.example.turismomalagaapp.ui.Submenu;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Switch;
 
+
+import com.example.turismomalagaapp.MenuLateralActivity;
 import com.example.turismomalagaapp.R;
+import com.example.turismomalagaapp.ui.map.MapFragment;
+
+import java.util.Locale;
 
 
 public class AjustesFragment extends Fragment {
 
     View view;
-
+    Locale local;
+    ImageButton botonEsp, botonIng;
+    private Configuration config = new Configuration();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_ajustes, container, false);
+        botonEsp = view.findViewById(R.id.imageButton);
+        botonIng = view.findViewById(R.id.imageButton2);
+
+
+        botonEsp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                local = new Locale("es");
+                config.locale = local;
+                getResources().updateConfiguration(config, null);
+            }
+        });
+
+        botonIng.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                local = new Locale("en");
+                config.locale = local;
+                getResources().updateConfiguration(config, null);
+            }
+        });
+
+
+
+
         return view;
-
-
     }
 
     @Override
@@ -30,4 +64,5 @@ public class AjustesFragment extends Fragment {
         super.onDestroy();
         //getActivity().getActionBar().setTitle();
     }
+
 }
